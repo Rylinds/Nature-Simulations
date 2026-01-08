fc4adc2b0d4d73f336cf9c9aa7fc18d8e6f948a8
Lindsey <lindseyboyer090@gmail.com>
2026-01-08 17:05:49 -0500

    Update mover.js

--- Oscillation/mover.js
+++ Oscillation/mover.js
@@ -8,6 +8,19 @@
 
 */
 
+let mover;
+
+function setup() {
+    createCanvas(1000, 1000);
+    mover = new Mover();
+}
+
+function draw() {
+    background(255);
+    mover.update();
+    mover.show();
+}
+
 class Mover {
 
     constructor() {
